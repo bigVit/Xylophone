f14b5bff4742b999056807674cfd17510fcbfa27
vitali <elmosmobile@gmail.com>
2018-07-11 16:42:56 +0300

    Initial commit

---
+++ app/src/main/java/com/vitali/xylophone/MainActivity.java
@@ -0,0 +1,107 @@
+package com.vitali.xylophone;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int NR_OF_SIMULATIONS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int PRIORITY = 0;
+    private final int LOOP = 0;
+    private final float RATE = 1.0f;
+
+    private SoundPool mSoundPool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSoundPool = new SoundPool(NR_OF_SIMULATIONS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
+    }
+
+
+    /**
+     * @param v
+     * play C sound
+     */
+    public void playC(View v){
+        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+
+    /**
+     * @param v
+     * play D sound
+     */
+    public void playD(View v){
+        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+    /**
+     *
+     * @param v
+     * play E sound
+     */
+
+    public void playE(View v){
+        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+
+    /**
+     * @param v
+     * play F sound
+     */
+    public void playF(View v){
+        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+
+    /**
+     * @param v
+     * play G sound
+     */
+    public void playG(View v){
+        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+
+    /**
+     * @param v
+     * play A sound
+     */
+    public void playA(View v){
+        mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+
+    /**
+     * @param v
+     * play B sound
+     */
+    public void playB(View v){
+        mSoundPool.play(mBSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, LOOP, RATE);
+    }
+
+}
